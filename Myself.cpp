b759e2bce9fd4daf325f9a5a953cb181b5fb967f
Mahad <mahadh714@gmail.com>
2026-09-17 17:33:08 +0500

    Add c++ variables and data types lesson

---
+++ Myself.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 18;
+    double height = 5.9;
+    char grade = 'A';
+    string name = "Mahad Hussain";
+    cout <<  "Name:" << name << endl;
+    cout << "Age:" << age << endl;
+    cout << "Height:" << height << endl;
+    cout << "Grade:" << grade << endl;
+    return 0;
+}
--- Untitled-2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-
-    cout<< "Hello World of coding iam getting into it and i will be a good coder in future" << endl;
-    return 0;
-
-}
