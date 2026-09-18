3b9b4a6c80f223b910d8c47933622a39c136b801
Mahad <mahadh714@gmail.com>
2026-09-18 21:44:30 +0500

    The profile one

---
+++ .vscode/profile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+   {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Your name is " << name << endl;
+   }
+    
+{
+    double height;
+    cout << "Enter your height: ";
+    cin >> height;
+    cout << "Your height is " << height << endl;
+}
+{
+
+    char ch ;
+    cout << "Enter your favourite character: ";
+    cin >> ch ;
+    
+    cout << "Your favourite character is: " << ch << endl;
+}
+    return 0;
+
+
+}
