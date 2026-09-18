fc8747db3e94bbf7f633c8964cd1353f006057d5
Mahad <mahadh714@gmail.com>
2026-09-18 16:21:29 +0500

    Add height program lesson

--- Myself.cpp
+++ Myself.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() 
 {
-    int age = 18;
-    double height = 5.9;
-    char grade = 'A';
-    string name = "Mahad Hussain";
-    cout <<  "Name:" << name << endl;
-    cout << "Age:" << age << endl;
-    cout << "Height:" << height << endl;
-    cout << "Grade:" << grade << endl;
+    double height;
+    cout << "enter your height\n";
+    cin >> height;
+
+    if (height > 5.0) 
+    {
+        cout << "You are not dwarf\n";
+    } 
+    else 
+    {
+        cout << "You are dwarf\n";
+    }
+
     return 0;
 }
